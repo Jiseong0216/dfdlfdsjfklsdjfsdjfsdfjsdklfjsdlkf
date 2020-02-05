00169846d0ebc3e88d367ae70b68495f158ead39
Jiseong0216 <60600022+Jiseong0216@users.noreply.github.com>
2020-02-05 12:36:38 +0900

    Add files via upload

---
+++ PacthNoteBot.py
@@ -0,0 +1,37 @@
+import discord
+import datetime
+import asyncio
+
+client = discord.Client()
+
+TOKEN = 'Njc0MTk3OTY3MDI4MzU1MDgy.XjovIA.DP1Av7dwPlwVRl4_Edv37JkNssI'
+
+now = datetime.datetime.now()
+nowDate = now.strftime('%Y-%m-%d')
+
+@client.event
+async def on_ready():
+    print('Bot start')
+    game = discord.Game("!도움 | " + nowDate + " 일")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!도움'):
+        embed = discord.Embed(color=0x555555)
+        embed.add_field(name="통합 Bot Information", value="V1 last updated 2020.02.05", inline=False)
+        embed.add_field(name="명령어 리스트 -------- command list", value="모든 명령어에는 느낌표를 붙입니다.", inline=False)
+        embed.add_field(name="!봇정보", value="", inline=False)
+        await message.channel.send(embed=embed)
+        
+    if message.content.startswith('.공지'):
+        embed = discord.Embed(color=0x555555)
+        embed.add_field(name="공지", value="[공지가 없습니다.]", inline=False)
+        await message.channel.send(embed=embed)
+
+    if message.content.startswith('!봇에탄생'):
+        embed = discord.Embed(color=0x555555)
+        embed.add_field(name="봇탄생", value="안녕? 나는 2020년 2월 4일에 지성이가 원래 봇이 죽었다고 급하게 내가 만들어졌어!", inline=False)
+        await message.channel.send(embed=embed)
+        
+client.run(TOKEN)
